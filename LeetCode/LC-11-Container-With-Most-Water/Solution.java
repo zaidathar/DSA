b14bcb917dc92c80454123ccdfcf404ce46b1b14
zaidathar <codewithzukon@gmail.com>
2023-05-10 18:13:00 +0530

    LeetCode 11 Solution

---
+++ LeetCode/LC-11-Container-With-Most-Water/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int l = 0, r = height.length -1 ;
+        int res = 0;
+        while(l<r){
+            int area = (r - l)*Math.min(height[l],height[r]);
+            res = Math.max(res,area);
+            if(height[l] < height[r]){
+                l++;
+            }
+            else r--;
+        }
+
+        return res;
+    }
+}
