e086c7c10d7147bce3729d9b6f5d49c336b8c482
zaidathar <codewithzukon@gmail.com>
2023-07-30 19:38:18 +0530

    Leetcode 27 Solution

---
+++ LeetCode/LC-27-Remove-Element/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        
+        int i =0 ;
+        for(int num : nums){
+            if(num!=val){
+                nums[i] = num;
+                i++;
+            }
+        }
+        
+        return i;
+    }
+}
