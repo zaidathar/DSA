867ad4e7a4eea1dd7f09497b4dba8e56e1d31867
zaidathar <codewithzukon@gmail.com>
2023-08-06 11:07:54 +0530

    GFG Anagram Solution

---
+++ GeeksForGeeks/GFG-Anagram/Solution.java
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    
+	public static void main (String[] args)throws IOException {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		while(t-->0)
+		{
+		    String str = br.readLine();
+		    String s1 = str.split(" ")[0];
+		    String s2 = str.split(" ")[1];
+		    
+		    Solution obj = new Solution();
+		    
+		    if(obj.isAnagram(s1,s2))
+		    {
+		        System.out.println("YES");
+		    }
+		    else
+		    {
+		         System.out.println("NO");
+		    }
+		    
+		    
+		    
+		}
+	}
+}// } Driver Code Ends
+
+
+class Solution
+{    
+    //Function is to check whether two strings are anagram of each other or not.
+    public static boolean isAnagram(String a,String b)
+    {
+        
+        // Your code here
+        if(a.length() != b.length())return false;
+        char[] aArr = a.toCharArray();
+        char[] bArr = b.toCharArray();
+        
+        Arrays.sort(aArr);
+        Arrays.sort(bArr);
+        for(int i = 0 ; i< aArr.length ; i++){
+            if(aArr[i]!=bArr[i]){
+               
+                return false;
+            }
+        }
+        
+        return true;
+        
+    }
+}
