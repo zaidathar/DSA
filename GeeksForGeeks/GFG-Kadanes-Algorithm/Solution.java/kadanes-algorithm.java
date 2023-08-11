0d91e65d0e0a46c32969ccfb9f06b3a78f570009
zaidathar <codewithzukon@gmail.com>
2023-08-11 12:21:58 +0530

    GFG Kadane Algorithm

---
+++ GeeksForGeeks/GFG-Kadanes-Algorithm/Solution.java/kadanes-algorithm.java
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+import java.io.*;
+
+class Main {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    //size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //adding elements
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling maxSubarraySum() function
+		    System.out.println(obj.maxSubarraySum(arr, n));
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long maxSubarraySum(int arr[], int n){
+        
+        int maxSoFar = Integer.MIN_VALUE;
+        int maxEndingHere = 0 ;
+        
+        for(int i : arr){
+            maxEndingHere +=i;
+            if(maxSoFar < maxEndingHere)maxSoFar=maxEndingHere;
+            if(maxEndingHere < 0)maxEndingHere = 0;
+            
+        }
+        
+        return maxSoFar;
+        
+    }
+    
+}
+
