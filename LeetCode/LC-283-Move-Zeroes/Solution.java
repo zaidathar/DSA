cd08ad6f866fd03d1596a506af05dd5306ef9957
zaidathar <codewithzukon@gmail.com>
2023-08-11 12:17:06 +0530

    LeetCode 283 Solution

---
+++ LeetCode/LC-283-Move-Zeroes/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int k = 0;
+        for(int i =0 ;i< nums.length;i++){
+            if(nums[i]!=0){
+                
+                nums[k]=nums[i];
+                k++;
+            }    
+        }
+        
+        for(int i = k ; i< nums.length ; i++){
+            nums[i]=0;
+        }
+    }
+}
