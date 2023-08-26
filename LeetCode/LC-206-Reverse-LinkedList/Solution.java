e70da905d9a7dcf090201bd3a5e6c438c09592ff
zaidathar <codewithzukon@gmail.com>
2023-08-26 14:53:46 +0530

    LeetCode 206 Solution

--- LeetCode/LC-206-Reverse-LinkedList/Solution.java
+++ LeetCode/LC-206-Reverse-LinkedList/Solution.java
@@ -1,5 +1,15 @@
 class Solution {
-    public ListNode reverseList(ListNode head) {
+
+    private static ListNode recursiveReverse(ListNode head){
+        if( head == null || head.next == null)
+            return head;
+        ListNode h = recursiveReverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return h;
+    }
+
+    private static ListNode iterativeReverse(ListNode head){
         ListNode h = null;
 
         while(head != null){
@@ -11,5 +21,9 @@ class Solution {
         }
 
         return h;
+    }
+    public ListNode reverseList(ListNode head) {
+       	// return iterativeReverse(head);
+	 return recursiveReverse(head);
     }
 }
