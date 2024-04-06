9f876ebf1db8b8a69911564722966b27eba9d455
zaidathar <codewithzukon@gmail.com>
2024-04-07 00:04:17 +0530

    LeetCode 1249 Solution

---
+++ LeetCode/LC-1249-Minimum-Remove-To-Make-Valid-Parenthesis/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        
+        Stack<Integer> stack = new Stack();
+        
+        for(int i = 0 ; i< sb.length(); i++){
+            if(sb.charAt(i) == '(')stack.add(i);
+            else if(sb.charAt(i) == ')'){
+                if(!stack.isEmpty())stack.pop();
+                else sb.setCharAt(i,'*');
+            }
+        }
+        
+        while(!stack.isEmpty()){
+            sb.setCharAt(stack.pop(), '*');
+            
+        }
+        
+        return sb.toString().replaceAll("\\*","");
+    }
+}
