d74a7703cf480181f338bc7f9e377bc64e53cfa2
zaidathar <codewithzukon@gmail.com>
2024-04-14 21:48:27 +0530

    LeetCode 73 brute force solution

--- LeetCode/LC-73-Set-Matrix-Zeroes/BruteForceSolution.java
+++ LeetCode/LC-73-Set-Matrix-Zeroes/BruteForceSolution.java
@@ -13,23 +13,13 @@ class Solution {
             }
         }
 
-
-        for(int i= 0 ; i < row ; i++){
-            if(rows[i]){
-                for(int c = 0 ; c < col ; c++){
-                    matrix[i][c]=0;
+        for(int i = 0 ; i < row ; i++){
+            for(int j = 0 ; j < col ; j++){
+                if( rows[i] || cols[j] ){
+                    matrix[i][j]=0;
                 }
-                rows[i]=false;
             }
         }
 
-        for(int i= 0 ; i < col ; i++){
-            if(cols[i]){
-                for(int r = 0 ; r < row ; r++){
-                    matrix[r][i]=0;
-                }
-                cols[i]=false;
-            }
-        }
     }
 }
